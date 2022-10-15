dc8a082952980c1fd7da8fbc77389141fe688a0f
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-10-15 10:09:30 +0530

    Response DTO created.

---
+++ inventory-service/src/main/java/com/vihanga/inventory/service/dto/InventoryResponseDto.java
@@ -0,0 +1,16 @@
+package com.vihanga.inventory.service.dto;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponseDto {
+    private String skuCode;
+    private Boolean isInStock;
+}
